6e3ae3e20ba12194172f7ff3edd51b534f3feba1
Kaiyrkhan <kairolla222@gmail.com>
2023-03-08 16:31:41 +0600

    http_headers_sent error SOLVED

--- app/pages/api/products/[id].js
+++ app/pages/api/products/[id].js
@@ -5,22 +5,27 @@ export default async function handler(req, res) {
   const {method} = req
   const id = req.query.id;
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(400).json({ error: 'Invalid ID' });
+    res.status(400).json({error: 'Invalid ID'});
     return;
   }
-  switch(method) {
+  switch (method) {
     case 'GET':
-      const get_product_by_id = await prisma.Product.findUnique({where : {id : id}});
-      if (!get_product_by_id) {
-        res.status(404).json({ error: 'Product Not Found' });
+      const get_product_by_id = await prisma.Product.findUnique({where: {id: id}});
+      if (get_product_by_id) {
+        res.status(200).json(get_product_by_id);
+      } else {
+        res.status(404).json({error: 'Product Not Found'});
       }
-      res.status(200).json(get_product_by_id);
+      break;
     case 'POST':
-      res.status(200).json({ method, name: "POST request"});
+      res.status(200).json({method, name: "POST request"});
+      break;
     case 'PUT':
-      res.status(200).json({ method, name: "PUT request"});
+      res.status(200).json({method, name: "PUT request"});
+      break;
     case 'DELETE':
-      res.status(200).json({ method, name: "DELETE request"});
+      res.status(200).json({method, name: "DELETE request"});
+      break;
     default:
       res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
       res.status(405).end('Method ${method} Not Allowed')
