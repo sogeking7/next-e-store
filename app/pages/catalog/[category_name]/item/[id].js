5233208b70bbfbaa39530482a3b070c4cc802cf5
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 03:16:50 +0600

    [fixed] removed layout paths

--- app/pages/catalog/[category_name]/item/[id].js
+++ app/pages/catalog/[category_name]/item/[id].js
@@ -1,6 +1,3 @@
-import CatalogHeader from '../../../../components/layouts/CatalogHeader'
-import Footer from "../../../../components/layouts/Footer";
-import MobileNavBar from "../../../../components/layouts/MobileNavBar";
 import ProductDetail from '../../../../components/pages/productId/product/ProductDetail'
 
 function Id() {
