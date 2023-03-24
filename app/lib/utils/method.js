34a50f12599af9b6cf0d70a1a55e3733d504d599
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 01:43:17 +0600

    [fixed] unslugify function error

--- app/lib/utils/method.js
+++ app/lib/utils/method.js
@@ -1,4 +1,1 @@
-export const unslugify = (slug)
-{
-  return slug.replace(/\-/g, " ").replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase());
-}
+export const unslugify = (slug) => slug.replace(/\-/g, " ").replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase());
