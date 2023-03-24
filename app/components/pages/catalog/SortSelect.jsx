93e8a54419042cc5a3177a7ba8eed5c8d4562310
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 01:39:49 +0600

    [created] created file SortSelect.jsx

---
+++ app/components/pages/catalog/SortSelect.jsx
@@ -0,0 +1,36 @@
+import {Group, Select} from "@mantine/core";
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+
+export default function SortSelect() {
+  const router = useRouter();
+  const [value, setValue] = useState(router.query.sort ? router.query.sort : "featured");
+
+  useEffect(() => {
+    const query = router.query;
+    query.sort = value;
+    router.push({
+      pathname: `/catalog/${query.category_name}`,
+      query: query,
+    }, undefined,  { shallow: true })
+  }, [value])
+
+  return (
+    <Group position="left">
+      <Select
+        radius="md"
+        value={value}
+        className="w-[140px]"
+        placeholder="Not set"
+        size="xs"
+        onChange={setValue}
+        data={[
+          {value: "desc", label: "Price: high to low"},
+          {value: "asc", label: "Price: low to high"},
+          {value: "name", label: "Alphabetically"},
+          {value: "featured", label: "Featured"},
+        ]}
+      />
+    </Group>
+  )
+}
