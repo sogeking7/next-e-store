ecf27fb280a20447cce5d74d59921e8c20b03cf8
Kaiyrkhan <kairolla222@gmail.com>
2023-03-24 23:15:17 +0600

    catalog header design adapted

--- app/components/layouts/CatalogHeader.jsx
+++ app/components/layouts/CatalogHeader.jsx
@@ -1,72 +1,54 @@
 import React from "react";
-
 import {
-  Container,
   Header,
   Flex,
   ActionIcon,
-  Box,
-  createStyles,
   Text,
 } from "@mantine/core";
-
 import Logo from "../ui/Logo";
-
-import { DarkModeButton } from "../ui/DarkModeButton";
-import {IconHeart, IconMenu2, IconShoppingCart} from "@tabler/icons";
-
+import {IconHeart, IconShoppingCart} from "@tabler/icons";
 import Search from "./Search";
 import AuthBtn from "../pages/auth/AuthBtn";
 import {useRouter} from "next/router";
 
-const useStyles = createStyles((theme) => ({
-  border: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-    borderBottom: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[4]}`
-  },
-}));
-
-function CatalogHeader({ setOpened, isFilterOn }) {
-  const { classes } = useStyles();
+function CatalogHeader({setOpened, isFilterOn}) {
   const router = useRouter();
+
   return (
-    <Header className={classes.border}>
-      <Container size="lg" className="my-2" sx={{height:'100%'}}>
-          <Flex className="flex md:flex-row flex-col  h-full first-letter:basic:flex-col basic:gap-2 md:gap-0 md:pt-0 md:pb-0 pb-[6px] basic:pt-4 md:items-center">
-            <Flex justify='space-between' className="lg:w-[23.2%] md:w-[28.5%] md:mr-4" align='center'>
-              <Logo />
-            </Flex>
-            <Flex className="justify-between lg:w-[76%] md:w-[70%] w-full">
-              <Search setOpened={setOpened} isFilterOn={isFilterOn}/>
-              <Flex className="gap-6 md:flex basic:hidden" align='center'>
-                <AuthBtn />
-                <Flex align="center" className="flex-col w-[50px]">
-                  <ActionIcon
-                    size="md"
-                    color='dark'
-                    variant="transparent"
-                  >
-                    <IconShoppingCart size={20} />
-                  </ActionIcon>
-                  <Text size={12} weight={600}>Cart</Text>
-                </Flex>
-                <Flex align="center" className="flex-col w-[50px]">
-                  <ActionIcon
-                    size="md"
-                    color='dark'
-                    variant="transparent"
-                    onClick={()=>{
-                      router.push('/user/wishlist')
-                    }}
-                  >
-                    <IconHeart size={20} />
-                  </ActionIcon>
-                  <Text size={12} weight={600}>Wishlist</Text>
-                </Flex>
-              </Flex>
+    <Header px="md" className="md:py-1 py-2 max-w-5xl mx-auto">
+      <Flex className="justify-between md:flex-row flex-col md:gap-0 gap-4">
+        <Logo/>
+        <Search setOpened={setOpened} isFilterOn={isFilterOn}/>
+        <Flex className="gap-4 items-center hidden md:flex">
+          <AuthBtn/>
+          <Flex align="center" className="cursor-pointer flex-col w-[50px]">
+            <ActionIcon
+              size="md"
+              color='dark'
+              variant="transparent"
+              onClick={() => {
+                router.push('/user/cart')
+              }}
+            >
+              <IconShoppingCart size={20}/>
+            </ActionIcon>
+            <Text size={12} weight={600}>Cart</Text>
+          </Flex>
+          <Flex align="center" className="cursor-pointer flex-col w-[50px]">
+            <ActionIcon
+              size="md"
+              color='dark'
+              variant="transparent"
+              onClick={() => {
+                router.push('/user/wishlist')
+              }}
+            >
+              <IconHeart size={20}/>
+            </ActionIcon>
+            <Text size={12} weight={600}>Wishlist</Text>
           </Flex>
         </Flex>
-      </Container>
+      </Flex>
     </Header>
   );
 }
