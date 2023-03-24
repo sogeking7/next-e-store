7116ed5bb468b00739f8526fde4194c69dc6e663
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 00:45:53 +0600

    [fixed] icon changed from IconMoonStar -> IconMoon;

--- app/components/ui/DarkModeButton.jsx
+++ app/components/ui/DarkModeButton.jsx
@@ -1,5 +1,5 @@
 import { ActionIcon, useMantineColorScheme } from '@mantine/core';
-import { IconSun, IconMoonStars } from '@tabler/icons';
+import {IconSun, IconMoon} from '@tabler/icons';
 
 export const DarkModeButton = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -13,7 +13,7 @@ export const DarkModeButton = () => {
       onClick={() => toggleColorScheme()}
       title="Toggle color scheme"
     >
-      {dark ? <IconSun size={20} /> : <IconMoonStars size={20} />}
+      {dark ? <IconSun size={20} /> : <IconMoon size={20} />}
     </ActionIcon>
   );
 }
