8aa3040dc3c102978f7d3eb3ee9018b445461f76
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 00:26:24 +0600

    [fixed] new status 'loading' component; new status 'unauthenticated' component; changed profile component

--- app/components/pages/auth/AuthBtn.js
+++ app/components/pages/auth/AuthBtn.js
@@ -1,22 +1,24 @@
-import {IconRefresh} from '@tabler/icons'
-import { useDisclosure } from '@mantine/hooks';
-import {Modal, Group, Flex} from '@mantine/core';
-import { useSession, signIn, signOut } from "next-auth/react";
-import {Box, Button, Text} from "@mantine/core";
+import {IconUser} from '@tabler/icons'
+import {useDisclosure} from '@mantine/hooks';
+import {Modal,Flex, ActionIcon, Loader, Text} from '@mantine/core';
+import {useSession, signIn} from "next-auth/react";
 import {AuthenticationForm} from "./AuthentificationForm";
 import Link from "next/link";
 import Image from "next/image";
+import React from "react";
 
 const AuthBtn = () => {
 
-  const [opened, { open, close }] = useDisclosure(false);
-  const { data: session, status } = useSession();
+  const [opened, {open, close}] = useDisclosure(false);
+  const {data: session, status} = useSession();
 
   if (status === "loading") {
     return (
-      <Flex className="items-center justify-center">
-        <IconRefresh className="icon animate-spin" />
+      <Flex align="center" onClick={open} className="cursor-pointer flex-col w-[50px]">
+        <Loader size="xs" className="relative top-1"/>
+        <Text size={12} className="relative top-1" weight={600}>Loading</Text>
       </Flex>
+
     );
   }
   if (status === "unauthenticated") {
@@ -25,19 +27,25 @@ const AuthBtn = () => {
         <Modal opened={opened} onClose={close} title={<Text className="text-2xl" weight={900}>
           Welcome to e-store</Text>
         }>
-          <AuthenticationForm signIn={signIn} />
+          <AuthenticationForm signIn={signIn}/>
         </Modal>
-        <Flex className="gap-2">
-          <Button variant="default" size="xs" radius="md" onClick={open}>Log in</Button>
-          <Button size="xs" radius="md" color="dark">Sign up</Button>
+        <Flex align="center" onClick={open} className="cursor-pointer flex-col w-[50px]">
+          <ActionIcon
+            size="md"
+            color='dark'
+            variant="transparent"
+          >
+            <IconUser size={20}/>
+          </ActionIcon>
+          <Text size={12} weight={600}>Sign in</Text>
         </Flex>
       </>
     );
   }
   return (
     <Link href="/user/profile">
-        <Flex className="flex-col justify-end items-center h-[50px] w-[50px] hover:cursor-pointer">
-        <Image src={session.user.image} alt={session.user.name} width={27} height={27} className="rounded-full" />
+      <Flex className="flex-col justify-center items-center h-[50px] w-[50px] hover:cursor-pointer">
+        <Image src={session.user.image} alt={session.user.name} width={26} height={26} className="rounded-full"/>
         <Text weight={600} size={12}>Profile</Text>
       </Flex>
     </Link>
