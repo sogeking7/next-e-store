92750da8b6625c981ccdba08281ca20fe0edc3c5
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 03:37:49 +0600

    [fixed] clear code; added ActionIcons from CatalogHeader;

--- app/components/layouts/MobileNavBar.jsx
+++ app/components/layouts/MobileNavBar.jsx
@@ -1,44 +1,58 @@
 import React from "react";
 import {
   Box,
+  Flex,
+  Text,
   ActionIcon,
   createStyles,
   MediaQuery,
+  Footer,
 } from "@mantine/core";
+import { useRouter } from 'next/router'
 import { IconHeart, IconShoppingCart, IconUser } from "@tabler/icons";
+import AuthBtn from "../pages/auth/AuthBtn";
 
 const useStyles = createStyles((theme) => ({
-  border: {
-    borderTop: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
-      }`,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : '#fff',
-    position: "fixed",
-    zIndex: 50,
-    bottom: 0,
-    padding: "4px",
-    display: "flex",
-    overflow: "hidden",
-    justifyContent: "space-between",
-    width: "100%",
-  },
+
 }));
 
 export default function MobileNavBar() {
   const { classes } = useStyles();
+  const router = useRouter();
   return (
-    <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-      <Box className={classes.border}>
-        <ActionIcon color="dark" size="xl" className="" variant="transparent">
-          <IconHeart />
-        </ActionIcon>
-        <ActionIcon color="dark" size="xl" className="" variant="transparent">
-          <IconShoppingCart />
-        </ActionIcon>
-        <ActionIcon color="dark" size="xl" variant="transparent">
-          <IconUser />
-        </ActionIcon>
-      </Box>
-    </MediaQuery>
+    <div className="md:hidden block">
+      <Footer className="fixed z-50 bottom-0 px-4 py-2 overflow-hidden">
+        <div className="flex justify-between">
+          <Flex align="center" className="cursor-pointer flex-col w-[50px]">
+            <ActionIcon
+              size="md"
+              color='dark'
+              variant="transparent"
+              onClick={() => {
+                router.push('/user/cart')
+              }}
+            >
+              <IconShoppingCart size={20} />
+            </ActionIcon>
+            <Text size={12} weight={600}>Cart</Text>
+          </Flex>
+          <Flex align="center" className="cursor-pointer flex-col w-[50px]">
+            <ActionIcon
+              size="md"
+              color='dark'
+              variant="transparent"
+              onClick={() => {
+                router.push('/user/wishlist')
+              }}
+            >
+              <IconHeart size={20} />
+            </ActionIcon>
+            <Text size={12} weight={600}>Wishlist</Text>
+          </Flex>
+          <AuthBtn />
+        </div>
+      </Footer>
+    </div>
   );
 }
 
