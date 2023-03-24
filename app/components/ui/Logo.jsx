d6ed479200a696556da4bf549d10cd1fd7eaf1e8
Kaiyrkhan <kairolla222@gmail.com>
2023-03-25 01:06:47 +0600

    [fixed] removed Flex @mantine/core; added cursor-pointer;

--- app/components/ui/Logo.jsx
+++ app/components/ui/Logo.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
-import {Flex, Title} from "@mantine/core"
+import {Title} from "@mantine/core";
 
 export default function Logo() {
   return (
-    <Flex className="min-w-max mr-8 items-center gap-2">
+    <div className="min-w-max mr-8 items-center gap-2">
       <Link href="/">
-        <Title>
+        <Title className="cursor-pointer">
           e-store
         </Title>
       </Link>
-    </Flex>
+    </div>
   )
 }
