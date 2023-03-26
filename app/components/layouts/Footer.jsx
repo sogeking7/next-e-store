bed1b39a74b5e3e17737a7d721ae6af10310d971
Kaiyrkhan <kairolla222@gmail.com>
2023-03-26 16:32:06 +0600

    [fixed] clear code; border stretched along full width;

--- app/components/layouts/Footer.jsx
+++ app/components/layouts/Footer.jsx
@@ -1,16 +1,18 @@
-import {Footer, Flex, Box} from "@mantine/core";
+import {Footer, Flex} from "@mantine/core";
 import SocialLinks from "./SocialLinks";
 import {DarkModeButton} from "../ui/DarkModeButton";
 
 export default function MyFooter() {
   return (
-    <Footer px="md" py="md" className="z-10 max-w-5xl mx-auto absolute bottom-0">
-      <Flex className="justify-between md:flex-row flex-col items-center">
-        <SocialLinks/>
-        <Box className="hidden md:block">
-          <DarkModeButton/>
-        </Box>
-      </Flex>
+    <Footer px="md" py="md" className="z-10 absolute bottom-0">
+      <div className="max-w-5xl mx-auto">
+        <Flex className="justify-between md:flex-row flex-col items-center">
+          <SocialLinks/>
+          <div className="hidden md:block">
+            <DarkModeButton/>
+          </div>
+        </Flex>
+      </div>
     </Footer>
   );
 }
