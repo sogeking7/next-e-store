785c085b8ea42dbae1910a5fb3b1ad9ac55be832
Kaiyrkhan <kairolla222@gmail.com>
2023-04-05 03:32:00 +0600

    fixed: session type set

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -7,11 +7,12 @@ import { NotificationsProvider } from "@mantine/notifications";
 import { RouterTransition } from "../components/layouts/RouterTransition";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import {Session} from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import Layout from '../components/layouts/Layout'
 import "../styles/index.css";
 
-export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+export default function App(props: AppProps<{session: Session}> & { colorScheme: ColorScheme }) {
   const { Component, pageProps: { session, ...pageProps } } = props
   const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
   const [queryClient] = useState(() => new QueryClient());
