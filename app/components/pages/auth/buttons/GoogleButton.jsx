0601e18eae1367e1b83eae66ff90a848ce570b44
Kaiyrkhan <kairolla222@gmail.com>
2023-04-05 03:13:22 +0600

    fixed: GoogleIcon moved into GoogleButton.jsx

--- app/components/pages/auth/buttons/GoogleButton.jsx
+++ app/components/pages/auth/buttons/GoogleButton.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import {Button} from "@mantine/core";
-import {GoogleIcon} from "./GoogleIcon";
+import {Button, Box} from "@mantine/core";
+import icon  from '../../../../public/GoogleIcon.ico'
+import Image from "next/image";
 
 export function GoogleButton(props) {
   return (
     <Button leftIcon={<GoogleIcon />} onClick={props.signIn} variant="default" radius="lg">
       Sign in with Google
     </Button>
+  )
+}
+
+export function GoogleIcon() {
+  return (
+    <Box className="w-5 h-5">
+      <Image src={icon}/>
+    </Box>
   )
 }
