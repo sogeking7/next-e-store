5d365be7f6c5dd68f0ff0d15ff525af74291cc45
Kaiyrkhan <kairolla222@gmail.com>
2023-04-05 15:10:20 +0600

    fixed: GET method

--- app/pages/user/wishlist/index.js
+++ app/pages/user/wishlist/index.js
@@ -1,15 +1,34 @@
-import {createStyles, Title} from "@mantine/core";
+import {createStyles, Text, Title} from "@mantine/core";
 import SideBar from "../../../components/pages/user/SideBar";
 import Head from "next/head";
+import {useQuery} from "react-query";
+import axios from "axios";
+import Loader from "../../../components/ui/Loader";
+import ProductCard from "../../../components/pages/catalog/product/ProductCard";
 
 
 const useStyle = createStyles((theme) => ({}));
 
 
 function Main(props) {
+  const { isLoading, error, data } = useQuery(['wishlist'], async () => {
+   return await axios.get('api/user/wishlist').then(res => res.data)
+  })
+  if (isLoading) return (
+    <div className="max-w-7xl mx-auto flex justify-center py-[20vh]">
+      <Loader size="lg" color="blue" variant="oval" />
+    </div>
+  )
+  if (error) return (
+    <Text weight={500}>Sorry, no products were found matching your criteria.</Text>
+  )
   return (
     <>
-      <Title className="mb-4" order={1}>Wishlist</Title>
+      {data.map((item) => {
+        return (
+          <ProductCard key={item.id} product={item}/>
+        )
+      })}
     </>
   );
 }
@@ -26,6 +45,7 @@ function Wishlist() {
       <div className="max-w-7xl mx-auto flex p-4">
         <SideBar/>
         <div className="md:px-4">
+          <Title className="mb-4" order={1}>Wishlist</Title>
           <Main/>
         </div>
       </div>
