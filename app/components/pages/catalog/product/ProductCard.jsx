c5ed59fe4fc9a53f135df6f2717579849d13792c
Kaiyrkhan <kairolla222@gmail.com>
2023-04-06 01:40:47 +0600

    added: Skeleton style; RemoveFromWishlistBtn; router -> next/link

--- app/components/pages/catalog/product/ProductCard.jsx
+++ app/components/pages/catalog/product/ProductCard.jsx
@@ -1,61 +1,112 @@
-import { Text, Button, createStyles, Rating, Group, Box } from "@mantine/core";
+import {Box, createStyles, Flex, Group, Rating, Skeleton, Text} from "@mantine/core";
 import Link from "next/link";
-import { AddToCart } from "./AddToCart";
+import {AddToCartBtn} from "./buttons/AddToCartBtn";
 import {useRouter} from "next/router";
+import React from "react";
+import {RemoveFromWishlistBtn} from "./buttons/RemoveFromWishlistBtn";
 
 const useStyles = createStyles((theme) => ({
-  wrapper: {
+  background: {
     backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+    borderRadius: theme.radius.md,
+  },
+  wrapper: {
     padding: '1rem .75rem',
     border: theme.colorScheme === "dark" ? `` : `1px solid ${theme.colors.gray[4]}`,
     borderRadius: theme.radius.md,
     ['@media (max-width: 768px)']: {
       padding: '1rem .5rem',
     }
+  },
+  wrapperSkeleton: {
+    padding: '1rem .75rem',
+    ['@media (max-width: 768px)']: {
+      padding: '1rem .5rem',
+    }
+  },
+  overlay: {
+    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
   }
 }));
 
-function ProductCard({layout, product}) {
+function ProductCard({layout, product, isLoading, url}) {
   const router = useRouter();
 
-  const { title, images, price, rating, id } = product;
-  const { classes } = useStyles();
-
-  const routerHandler = (id) => {
-    const query = router.query;
-    query.id = id;
-    router.push(`/catalog/${router.query.category_name}/item/${id}`)
-   }
+  const {classes} = useStyles();
 
   return (
-    <Box className={layout ? "flex flex-row gap-4" : "flex flex-col gap-2"}>
-      <div className={classes.wrapper}>
-        <div className={layout ? "w-1/2" : "aspect-square flex items-center bg-white mb-2"}>
-          <img
-            onClick={() => routerHandler(id)}
-            className={layout ? 'object-contain' : 'object-contain h-[160px] w-full'}
-            src={images[0]}
-            alt={null}
-          />
-        </div>
-        <Box className={layout ? "" : "flex flex-col gap-2"}>
-          <Box>
-            <Text onClick={() => routerHandler(id)} lineClamp={2} size='sm' className="cursor-pointer hover:text-[#228be6]">
-              {title}
-            </Text>
-            <Link href="/">
-              <Rating defaultValue={rating} size="xs" fractions={2} readOnly className="" />
-            </Link>
-          </Box>
-          <div className="flex justify-between">
-            <Text weight="bold">
-              {price}$
-            </Text>
-            <AddToCart/>
+    <div className={isLoading ? null : classes.background}>
+      <div className={isLoading ? classes.wrapperSkeleton : classes.wrapper}>
+        <Box className={layout ? "flex flex-row gap-4" : "flex flex-col gap-2"}>
+          <div className={isLoading ? null : classes.overlay}>
+            <div className={`aspect-square flex items-center mb-2 ${layout && 'h-[160px]'}`}>
+              {
+                isLoading ?
+                  <Skeleton height={layout ? 160 : 200} width={layout ? 160 : 200} radius="md"/>
+                  :
+                  <Link href={`/catalog/${product.categoryName}/item/${product.id}`}>
+                    <img
+                      className='object-contain w-full'
+                      src={product.images[0]}
+                      alt={null}
+                    />
+                  </Link>
+              }
+            </div>
+          </div>
+          <div className="flex flex-col justify-between w-full">
+            <Flex direction='column' gap={4}>
+              <div>
+                {
+                  isLoading ?
+                    <Skeleton height={20} width="100%" mb={4}/>
+                    :
+                    <Link href={`/catalog/${product.categoryName}/item/${product.id}`}>
+                      <Text lineClamp={2} size='sm'
+                            className="cursor-pointer hover:text-[#228be6] mb-1">
+                        {product.title}
+                      </Text>
+                    </Link>
+                }
+
+                <Link href="/">
+                  {
+                    isLoading ?
+                      <Skeleton height={20} width="100%"/>
+                      :
+                      <Rating defaultValue={product.rating} size="xs" fractions={2} readOnly className=""/>
+                  }
+                </Link>
+              </div>
+              <Flex justify="space-between">
+                <Text weight="bold">
+                  {
+                    isLoading ?
+                      <Skeleton height={20} width={100}/>
+                      :
+                      '$' + product.price
+                  }
+                </Text>
+                {
+                  layout ?
+                    <></>
+                    :
+                    <AddToCartBtn/>
+                }
+              </Flex>
+            </Flex>
+            {
+              layout && !isLoading ?
+                <Group position="right">
+                  <RemoveFromWishlistBtn productId={product.id}/>
+                </Group>
+                :
+                <></>
+            }
           </div>
         </Box>
       </div>
-    </Box>
+    </div>
   );
 }
 
