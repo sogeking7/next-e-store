340bf87b91415d3044194d6c81caf46af1dc727a
Kaiyrkhan <kairolla222@gmail.com>
2023-04-05 13:31:54 +0600

    fixed: ProfileContent.jsx removed

--- app/pages/user/profile/index.js
+++ app/pages/user/profile/index.js
@@ -1,22 +1,45 @@
-import { createStyles, Flex} from "@mantine/core";
-import {useState} from "react";
+import {Box, Container, createStyles, Flex, Text, Title} from "@mantine/core";
+import React, {useState} from "react";
 import SideBar from "../../../components/pages/user/SideBar";
-import ProfileContent from "../../../components/pages/user/profile/ProfileContent";
+import {useQuery} from "react-query";
+import {getSession} from "next-auth/react";
+import Loader from "../../../components/ui/Loader";
+import Image from "next/image";
 
-const useStyle = createStyles((theme) => ({
-}));
+const useStyle = createStyles((theme) => ({}));
 
-function Profile() {
-  const { classes } = useStyle();
-  const [opened, setOpened] = useState(false);
+function Main() {
+  const {isLoading, error, data: session} = useQuery('session', async () => {
+    return await getSession();
+  })
+
+  if (isLoading) return (
+    <Container size="lg" className="flex justify-center p-[20vh]">
+      <Loader size="lg" color="blue" variant="oval"/>
+    </Container>
+  )
+  if (error) return 'An error has occurred: ' + error.message
 
   return (
+    <Box className="w-[80%] md:px-4 pt-4">
+
+      <Box mb={2}>
+        <Box mb={8}>
+          <Image src={session.user.image} alt={session.user.name} width={100} height={100} className="rounded-full"/>
+        </Box>
+        <Title>{session.user.name}</Title>
+      </Box>
+
+    </Box>
+  );
+}
+
+function Profile() {
+  return (
     <>
-      <div className="max-w-7xl mx-auto p-4">
-        <Flex>
-          <SideBar/>
-          <ProfileContent/>
-        </Flex>
+      <div className="max-w-7xl mx-auto flex p-4">
+        <SideBar/>
+        <Main/>
       </div>
     </>
   );
