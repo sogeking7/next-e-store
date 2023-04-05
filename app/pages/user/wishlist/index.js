20a75572aa4445af5353defc6309f1791a98436c
Kaiyrkhan <kairolla222@gmail.com>
2023-04-06 01:46:04 +0600

    added: Skeleton style

--- app/pages/user/wishlist/index.js
+++ app/pages/user/wishlist/index.js
@@ -1,41 +1,65 @@
 import {createStyles, Text, Title} from "@mantine/core";
 import SideBar from "../../../components/pages/user/SideBar";
 import Head from "next/head";
-import {useQuery} from "react-query";
+import {useMutation, useQuery} from "react-query";
 import axios from "axios";
 import Loader from "../../../components/ui/Loader";
 import ProductCard from "../../../components/pages/catalog/product/ProductCard";
 
 
-const useStyle = createStyles((theme) => ({}));
+const useStyle = createStyles((theme) => ({
+  wrapper: {
+    width: '50%',
+    padding: '0 1rem',
+    ['@media (max-width: 1024px)']: {
+      width: '66.5%'
+    },
+    ['@media (max-width: 767px)']: {
+      padding: '0',
+      width: '100%'
+    }
+  },
+  title: {
+    fontSize: '2rem',
+    fontFamily: 'sans-serif',
+    fontWeight: 600,
+    ['@media (max-width: 767px)']: {
+      fontSize: '1.5rem'
+    },
+    marginBottom: '1rem'
+  }
+}));
 
 
 function Main(props) {
-  const { isLoading, error, data } = useQuery(['wishlist'], async () => {
-   return await axios.get('api/user/wishlist').then(res => res.data)
+  const {isLoading, error, data} = useQuery(['wishlist'], async () => {
+    return await axios.get('/api/user/wishlist').then(res => res.data)
   })
+
   if (isLoading) return (
-    <div className="max-w-7xl mx-auto flex justify-center py-[20vh]">
-      <Loader size="lg" color="blue" variant="oval" />
+    <div className="w-full">
+      <ProductCard isLoading={1} product={undefined} layout={1}/>
+      <ProductCard isLoading={1} product={undefined} layout={1}/>
+      <ProductCard isLoading={1} product={undefined} layout={1}/>
     </div>
   )
   if (error) return (
     <Text weight={500}>Sorry, no products were found matching your criteria.</Text>
   )
   return (
-    <>
+    <div className="w-full">
       {data.map((item) => {
         return (
-          <ProductCard key={item.id} product={item}/>
+          <ProductCard key={item.id} product={item} layout={1} isLoading={0} url={"user/wishlist"}/>
         )
       })}
-    </>
+    </div>
   );
 }
 
 
 function Wishlist() {
-
+  const {classes} = useStyle();
   return (
     <>
       <Head>
@@ -44,8 +68,8 @@ function Wishlist() {
       </Head>
       <div className="max-w-7xl mx-auto flex p-4">
         <SideBar/>
-        <div className="md:px-4">
-          <Title className="mb-4" order={1}>Wishlist</Title>
+        <div className={classes.wrapper}>
+          <Text className={classes.title} >Wishlist</Text>
           <Main/>
         </div>
       </div>
