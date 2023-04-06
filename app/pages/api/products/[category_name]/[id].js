b60305db66d75d4b7ff677036b0e000ff3555da9
Kaiyrkhan <kairolla222@gmail.com>
2023-04-06 14:25:27 +0600

    fixed: clear code

--- app/pages/api/products/[category_name]/[id].js
+++ app/pages/api/products/[category_name]/[id].js
@@ -1,38 +1,52 @@
 import prisma from '../../../../lib/prisma'
+import {getSession} from "next-auth/react";
 
 export default async function handler(req, res) {
   const {method} = req
-  const productId = req.query.id;
-  const categoryName = req.query.category_name;
+  const {productId, categoryName} = req.query;
+
+  const session = await getSession({req});
 
   switch (method) {
     case 'GET':
-      const get_category = await prisma.Category.findUnique({
+
+      const category = await prisma.Category.findUnique({
         where: {
           name: categoryName
         }
       })
-      if (!get_category) {
-        return res.status(404).json({error: 'Page Not Found'});
-      }
-      const get_product_by_id = await prisma.Category.findUnique({
+
+      if (!category) return res.status(404).json();
+
+      const allProducts = await prisma.Category.findUnique({
         where: {
-          id: get_category.id
+          id: category.id
         },
         include: {
           products: {
             where: {
               id: productId
             },
+            include: {
+              category: {
+                select: {
+                  name: true
+                }
+              }
+            }
           },
         },
-      })
-      get_product_by_id.products[0].categoryName = categoryName;
-      if (get_product_by_id) {
-        res.status(200).json(get_product_by_id.products[0]);
-      } else {
-        res.status(404).json({error: 'Product Not Found'});
-      }
+      });
+      const wishlist = session ? await prisma.user.findUnique({
+          where: { id: session.user.id },
+          select: { wishlist: { select: { id: true } } },
+        }).then((user) => user.wishlist.some((p) => p.id === productId)) :0
+
+      allProducts.products[0].wishlist = wishlist;
+
+      if (allProducts) res.status(200).json(allProducts.products[0]);
+      res.status(404).json({error: 'Product Not Found'});
+
       break;
     case 'POST':
       res.status(200).json({method, name: "POST request"});
