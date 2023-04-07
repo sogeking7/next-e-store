b05f5e1bb868204eb6105c0c6807ae14c66626eb
Kaiyrkhan <kairolla222@gmail.com>
2023-04-07 18:27:44 +0600

    fixed: set Link instead of router()

--- app/components/layouts/CatalogHeader.jsx
+++ app/components/layouts/CatalogHeader.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import {
-  Header,
-  Flex,
-  ActionIcon,
-  Text,
-} from "@mantine/core";
+import {ActionIcon, Flex, Header, Text,} from "@mantine/core";
 import Logo from "../ui/Logo";
 import {IconHeart, IconShoppingCart} from "@tabler/icons";
 import Search from "./Search";
 import AuthBtn from "../pages/auth/buttons/AuthBtn";
 import {useRouter} from "next/router";
 import {DarkModeButton} from "../ui/DarkModeButton";
+import Link from "next/link";
 
 function CatalogHeader({setOpened, isFilterOn}) {
   const router = useRouter();
@@ -18,45 +14,43 @@ function CatalogHeader({setOpened, isFilterOn}) {
   return (
     <Header className="py-2">
       <div className="max-w-7xl mx-auto">
-      <Flex className="justify-between items-center flex-row md:gap-0 gap-4 px-4">
-        <Logo/>
-        <div className="md:hidden">
-          <DarkModeButton/>
-        </div>
-        <Search setOpened={setOpened} isFilterOn={isFilterOn}/>
-        <Flex className="gap-4 items-center hidden md:flex">
-          <AuthBtn/>
-          <Flex align="center" className="cursor-pointer flex-col w-[50px]">
-            <ActionIcon
-              size="md"
-              color='dark'
-              variant="transparent"
-              onClick={() => {
-                router.push('/user/cart')
-              }}
-            >
-              <IconShoppingCart size={20}/>
-            </ActionIcon>
-            <Text size={12} weight={600}>Cart</Text>
-          </Flex>
-          <Flex align="center" className="cursor-pointer flex-col w-[50px]">
-            <ActionIcon
-              size="md"
-              color='dark'
-              variant="transparent"
-              onClick={() => {
-                router.push('/user/wishlist')
-              }}
-            >
-              <IconHeart size={20}/>
-            </ActionIcon>
-            <Text size={12} weight={600}>Wishlist</Text>
+        <Flex className="justify-between items-center flex-row md:gap-0 gap-4 px-4">
+          <Logo/>
+          <div className="md:hidden">
+            <DarkModeButton/>
+          </div>
+          <Search setOpened={setOpened} isFilterOn={isFilterOn}/>
+          <Flex className="gap-4 items-center hidden md:flex">
+            <AuthBtn/>
+            <Link href='/cart'>
+              <Flex align="center" className="cursor-pointer flex-col w-[50px]">
+                <ActionIcon
+                  size="md"
+                  color='dark'
+                  variant="transparent"
+                >
+                  <IconShoppingCart size={20}/>
+                </ActionIcon>
+                <Text size={12} weight={600}>Cart</Text>
+              </Flex>
+            </Link>
+            <Link href='/user/wishlist'>
+              <Flex align="center" className="cursor-pointer flex-col w-[50px]">
+                <ActionIcon
+                  size="md"
+                  color='dark'
+                  variant="transparent"
+                >
+                  <IconHeart size={20}/>
+                </ActionIcon>
+                <Text size={12} weight={600}>Wishlist</Text>
+              </Flex>
+            </Link>
           </Flex>
         </Flex>
-      </Flex>
       </div>
     </Header>
-  );
+  )
 }
 
 export default CatalogHeader;
