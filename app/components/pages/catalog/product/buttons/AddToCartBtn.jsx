79da8fa66a18c243f5970149d074e37cda7682f5
Kaiyrkhan <kairolla222@gmail.com>
2023-04-07 18:30:28 +0600

    added: button handler

--- app/components/pages/catalog/product/buttons/AddToCartBtn.jsx
+++ app/components/pages/catalog/product/buttons/AddToCartBtn.jsx
@@ -1,21 +1,89 @@
-import {ActionIcon, Tooltip} from "@mantine/core";
-import {IconShoppingCartPlus} from "@tabler/icons";
+import {Button, Group} from "@mantine/core";
+import {IconSquareRoundedPlus} from "@tabler/icons";
+import React, {useState} from "react";
+import axios from "axios";
+import {useMutation, useQueryClient} from "react-query";
+import {showNotification} from "@mantine/notifications";
+import {IconSquareRoundedCheckFilled} from "@tabler/icons-react";
 
-export const AddToCartBtn = () => {
+export const useAddItemToCart = (setDeletionError) => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    async (id) => {
+      return await axios.post(`/api/cart/${id}`);
+    },
+    {
+      onSuccess: async () => {
+        await queryClient.cancelQueries(["cart"]);
+        await queryClient.invalidateQueries(["cart"]);
+        await showNotification({
+          id: 'load-data',
+          color: 'teal',
+          title: 'Added To Wishlist',
+          autoClose: 2000,
+        })
+      },
+      onError: ({message}) => {
+        setDeletionError(message);
+      },
+    }
+  );
+};
+export const useDeleteItemFromCart = (setDeletionError) => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    async (id) => {
+      return await axios.delete(`/api/cart/${id}`);
+    },
+    {
+      onSuccess: async () => {
+        await queryClient.cancelQueries(["cart"]);
+        await queryClient.invalidateQueries(["cart"]);
+        await showNotification({
+          id: 'load-data',
+          color: 'teal',
+          title: 'Deleted From Cart',
+          autoClose: 2000,
+        })
+      },
+      onError: ({message}) => {
+        setDeletionError(message);
+      },
+    }
+  );
+};
+
+export const AddToCartBtn = ({productId, isInCart}) => {
+  const [active, setActive] = useState(isInCart);
+
+
+  const {mutate: mutateAdd, isLoading: isAdding} = useAddItemToCart();
+  const handleAddClick = (id) => {
+    mutateAdd(id);
+  }
+  const {mutate, isLoading: isDeleting} = useDeleteItemFromCart();
+  const handleDeleteClick = (id) => {
+    mutate(id);
+  };
+
   return (
-    <Tooltip
-      label="Add to Cart"
-      position="top"
-      withArrow
-      color="blue"
-    >
-      <ActionIcon
-        size="lg"
-        variant="subtle"
-        className='hover:text-blue-500 text-gray-500'
+    <Group>
+      <Button
+        rightIcon={active ? <IconSquareRoundedCheckFilled size={20}/> : <IconSquareRoundedPlus size={20}/>}
+        size="xs"
+        radius="xl"
+        color={active ? "green.5" : "indigo.5"}
+        onClick={() => {
+          // active ? handleDeleteClick(productId) : handleAddClick(productId)
+          setActive(!active);
+        }}
+        loading={isAdding || isDeleting}
+        loaderPosition="right"
       >
-        <IconShoppingCartPlus size={20}/>
-      </ActionIcon>
-    </Tooltip>
+        {
+          active ? 'Added to Cart' : 'Add to Cart'
+        }
+      </Button>
+    </Group>
   );
 };
