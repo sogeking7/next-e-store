8479ea812ed6ec3db1aa9ee0eb883f6f4cbb6a17
Kaiyrkhan <kairolla222@gmail.com>
2023-04-10 17:51:17 +0600

    created: product.inWishlist

--- app/pages/api/products/[category_name]/[id].js
+++ app/pages/api/products/[category_name]/[id].js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   const {id: productId, category_name: categoryName} = req.query;
 
   const session = await getSession({req});
-  // const {user: {id: userId}} = session;
 
   switch (method) {
     case 'GET':
@@ -16,8 +15,16 @@ export default async function handler(req, res) {
         }
       })
       if (!category) {
-        return res.status(404).json();
+        return res.status(404).json("Page Not Found");
       }
+      const user = await prisma.user.findUnique({
+        where: {
+          id: session.user.id
+        },
+        include: {
+          wishlist: true
+        }
+      })
       const get_product_by_id = await prisma.Category.findUnique({
         where: {
           id: category.id
@@ -28,6 +35,7 @@ export default async function handler(req, res) {
               id: productId
             },
             include: {
+              wishlist: true,
               category: {
                 select: {
                   name: true
@@ -37,24 +45,10 @@ export default async function handler(req, res) {
           },
         },
       });
-
       if (get_product_by_id) {
-        const productExistsInWishlist = session ? await prisma.user.findUnique({
-          where: {id: session.user.id},
-          select: {wishlist: {select: {id: true}}},
-        }).then((user) => user.wishlist.some((p) => p.id === productId)) : false
+        const product = get_product_by_id.products[0];
 
-        // const productExistsInCart = false
-        const productExistsInCart = session ? await prisma.user.findUnique({
-          where: {id: session.user.id},
-          include: {
-            cart: true
-          }
-        }).then((user) => user.cart.some((p) => p.id === productId)) : false
-
-        get_product_by_id.products[0].inWishlist = productExistsInWishlist;
-        get_product_by_id.products[0].inCart = productExistsInCart;
-
+        product.inWishlist = !!product.wishlistIDs.includes(user.wishlist.id);
         return res.status(200).json(get_product_by_id.products[0]);
       } else {
         return res.status(404).json({error: 'Product Not Found'});
