a4c5135e23be331d910f00adf83f73b5588d9a29
Kaiyrkhan <kairolla222@gmail.com>
2023-04-12 17:55:16 +0600

    removed

---
+++ app/components/layouts/mobile/MobileLayout.jsx
@@ -0,0 +1,30 @@
+import { createStyles } from '@mantine/core';
+import CatalogHeader from './CatalogHeader';
+import MyFooter from './Footer';
+import MobileNavBar from './mobile/MobileNavBar';
+
+const useStyle = createStyles((theme) => ({
+  wrapper: {
+    minHeight: '100vh',
+    ['@media (max-width: 768px)']: {
+      marginBottom: '67px',
+    },
+    paddingBottom: '61px',
+    position: 'relative',
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  }
+}));
+
+const MobileLayout = ({ children }) => {
+	const { classes } = useStyle();
+
+	return (
+    <div className={classes.wrapper}>
+			<CatalogHeader/>
+			<MobileNavBar/>
+      {children}
+			<MyFooter/>
+    </div>
+  );
+};
+export default MobileLayout
